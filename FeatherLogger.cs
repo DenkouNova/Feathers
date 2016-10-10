d189400d856a986dc57dd1194bf26b095ae5fc05
fggallant <fggallant@gmail.com>
2016-10-10 10:13:28 -0400

    added FeatherLogger - not using configuration file yet

---
+++ FeatherLogger.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Feathers
+{
+    public class FeatherLogger
+    {
+        public const string TAB_STRING = "  "; // 2-space tab
+
+        public const int TRACE_LEVEL_EXTREME = 10;
+        public const int TRACE_LEVEL_INFO = 9;
+        public const int TRACE_LEVEL_SQL = 6;
+        public const int TRACE_LEVEL_WARN = 3;
+        public const int TRACE_LEVEL_ERROR = 1;
+        public const int TRACE_LEVEL_NOTHING = int.MinValue;
+
+        public int TraceLevel { get; set; }
+        public string FullPath { get { return m_strFolderName + @"\" + m_strFilename; } }
+        public string ErrorMessage { get; set; } // Contains text when the logger was created in some wrong state
+
+        private int m_iTabLevel;
+        private string m_strFolderName, m_strFilename;
+
+
+        public FeatherLogger()
+        {
+            ErrorMessage = "";
+            TraceLevel = TRACE_LEVEL_NOTHING;
+        }
+
+        public FeatherLogger(int p_iTraceLevel, string p_strFoldername, string p_strFilename, bool p_blnTimestampInFilename, string p_strExtension)
+        {
+            ErrorMessage = "";
+            TraceLevel = TRACE_LEVEL_NOTHING;
+
+            try
+            {
+                this.TraceLevel = p_iTraceLevel;
+
+                m_strFolderName = !String.IsNullOrEmpty(p_strFoldername) ? p_strFoldername :
+                    Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+                m_strFilename = p_strFilename + (p_blnTimestampInFilename ? DateTime.Now.ToString("yyyyMMddhhmmss") : "") +
+                    (String.IsNullOrEmpty(p_strExtension) ? "" : "." + p_strExtension);
+
+                m_iTabLevel = 0;
+
+                if (!Directory.Exists(m_strFolderName)) Directory.CreateDirectory(m_strFolderName);
+
+                WriteOneLine("<!-- Logger starting -->");
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = "EXCEPTION: " + ex.Message +
+                    ((ex.InnerException == null) ? "" : " / INNER EXCEPTION: " + ex.InnerException.Message);
+            }
+
+        }
+
+        public void Error(string s)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_ERROR) WriteOneLine("ERROR: " + s);
+        }
+
+        public void Error(Exception ex)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_ERROR)
+            {
+                WriteOneLine("ERROR: Got exception '" + ex.Message + "'");
+                if (ex.InnerException != null)
+                {
+                    WriteOneLine("ERROR: Got inner exception '" +
+                        ex.InnerException.Message + "'");
+                }
+            }
+        }
+
+        public void Warn(string s)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_WARN) WriteOneLine("WARN : " + s);
+        }
+
+        public void Sql(string s)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_SQL) WriteOneLine("S Q L: " + s);
+        }
+
+        public void Info(string s)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_INFO) WriteOneLine("INFO : " + s);
+        }
+
+        public void Extreme(string s)
+        {
+            if (this.TraceLevel >= TRACE_LEVEL_EXTREME) WriteOneLine("EXTRM: " + s);
+        }
+
+        public void OpenSection(string s)
+        {
+            WriteOneLine("<" + s + "> " + GenerateTimestamp());
+            m_iTabLevel++;
+        }
+
+        public void CloseSection(string s)
+        {
+            m_iTabLevel--;
+            WriteOneLine("</" + s + "> " + GenerateTimestamp());
+        }
+
+        private void WriteOneLine(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < m_iTabLevel; i++) sb.Append(TAB_STRING);
+            sb.Append(s);
+            using (StreamWriter sw = new StreamWriter(m_strFolderName + @"\" + m_strFilename, true, Encoding.UTF8))
+            {
+                sw.WriteLine(FeatherStrings.ReplaceWhitespaceSpecialCharacters(sb.ToString()));
+            }
+        }
+
+        private string GenerateTimestamp()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            // This way of calculating milliseconds doesn't work much, sometimes gives values higher than 1000
+            //    (Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) -
+            //    (Convert.ToInt32(DateTime.Now.TimeOfDay.TotalSeconds) * 1000));
+        }
+
+
+
+        // This uses the app.config from the calling exe
+        /*
+        public static FeatherLogger CreateFeatherLoggerFromConfigFile()
+        {
+            FeatherLogger fl = new FeatherLogger(); // logs nothing
+            string strTraceLevel, strLogFolder, strLogFilename, strTimestamp;
+
+            try
+            {
+                strTraceLevel = ConfigurationManager.AppSettings.Get("TraceLevel");
+                strLogFolder = ConfigurationManager.AppSettings.Get("LogFolder");
+                strLogFilename = ConfigurationManager.AppSettings.Get("LogFilename");
+                strTimestamp = ConfigurationManager.AppSettings.Get("TimestampInFilename");
+                if (!String.IsNullOrEmpty(strTraceLevel) &&
+                    !String.IsNullOrEmpty(strLogFilename) &&
+                    !String.IsNullOrEmpty(strTimestamp))
+                {
+                    // Note that LogFolder can be null/empty, in that case we use the running exe path
+                    fl = new FeatherLogger(
+                        Convert.ToInt32(strTraceLevel),
+                        strLogFolder,
+                        strLogFilename,
+                        strTimestamp.Equals("TRUE"),
+                        "xml"
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                fl.ErrorMessage = "EXCEPTION: " + ex.Message +
+                    ((ex.InnerException == null) ? "" : " / INNER EXCEPTION: " + ex.InnerException.Message);
+            }
+
+            return fl;
+        }
+        */
+
+
+    }
+}
---
+++ Feathers_Tests/TestFeatherLogger.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Feathers;
+
+namespace Feathers_Tests
+{
+    /// <summary>
+    /// Summary description for FeatherLogger_UnitTests
+    /// </summary>
+    [TestClass]
+    public class FeatherLogger_UnitTests
+    {
+        static string TEST_PATH = @"D:\Logger\Test_FeatherLogger";
+
+        // Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            if (Directory.Exists(TEST_PATH)) Directory.Delete(TEST_PATH, true);
+        }
+
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+
+        [TestMethod]
+        public void Test_FeatherLoggerCtorWithoutTimestamp()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+
+            FeatherLogger fl = new FeatherLogger(
+                FeatherLogger.TRACE_LEVEL_INFO,
+                TEST_PATH + @"\" + testName,
+                testName,
+                false,
+                "xml");
+
+            Assert.IsTrue(Directory.Exists(TEST_PATH + @"\" + testName));
+            Assert.IsFalse(!File.Exists(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"));
+        }
+
+        [TestMethod]
+        public void Test_FeatherLoggerCtorWithTimestamp()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+
+            FeatherLogger fl = new FeatherLogger(
+                FeatherLogger.TRACE_LEVEL_INFO,
+                TEST_PATH + @"\" + testName,
+                testName,
+                true,
+                "xml");
+
+            int numberOfFilesCreatedWithTimestamp =
+                Directory.GetFiles(TEST_PATH + @"\" + testName, testName + "20*.xml*").Length;
+            int numberOfFilesCreatedTotal = 
+                Directory.GetFiles(TEST_PATH + @"\" + testName, testName + "*").Length;
+
+            Assert.IsTrue(numberOfFilesCreatedWithTimestamp == 1);
+            Assert.IsFalse(numberOfFilesCreatedTotal != numberOfFilesCreatedWithTimestamp);
+        }
+
+
+        ///<summary>
+        ///Test section opening and closing with tabs
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerOpenCloseSection()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string sectionName = "section";
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_INFO, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.OpenSection(sectionName);
+            fl.Error(logString);
+            fl.OpenSection(sectionName);
+            fl.Error(logString);
+            fl.OpenSection(sectionName);
+            fl.Error(logString);
+            fl.CloseSection(sectionName);
+            fl.Error(logString);
+            fl.CloseSection(sectionName);
+            fl.Error(logString);
+            fl.CloseSection(sectionName);
+            fl.Error(logString);
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine(); // ignore first line, it's just an intro
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, sectionName.Length + 2),
+                    "<" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length + FeatherLogger.TAB_STRING.Length),
+                    FeatherLogger.TAB_STRING + "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, FeatherLogger.TAB_STRING.Length + sectionName.Length + 2),
+                    FeatherLogger.TAB_STRING + "<" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length + FeatherLogger.TAB_STRING.Length * 2),
+                    FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, FeatherLogger.TAB_STRING.Length * 2 + sectionName.Length + 2),
+                    FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + "<" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length + FeatherLogger.TAB_STRING.Length * 3),
+
+                    FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + "ERROR: " + logString);
+
+                Assert.AreEqual(sr.ReadLine().Substring(0, FeatherLogger.TAB_STRING.Length * 2 + sectionName.Length + 2 + 1),
+                    FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + "</" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length + FeatherLogger.TAB_STRING.Length * 2),
+                    FeatherLogger.TAB_STRING + FeatherLogger.TAB_STRING + "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, FeatherLogger.TAB_STRING.Length + sectionName.Length + 2 + 1),
+                    FeatherLogger.TAB_STRING + "</" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length + FeatherLogger.TAB_STRING.Length),
+                    FeatherLogger.TAB_STRING + "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, sectionName.Length + 2 + 1),
+                    "</" + sectionName + ">");
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+            }
+        }
+
+
+
+        ///<summary>
+        ///Test that Nothing-level tracing traces nothing
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerNothing()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_NOTHING, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+                Assert.IsNull(sr.ReadLine());
+            }
+        }
+
+
+
+        ///<summary>
+        ///Test that error tracing only traces errors
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerError()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_ERROR, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.IsNull(sr.ReadLine());
+            }
+        }
+
+
+
+        ///<summary>
+        ///Test that warn tracing traces both errors and warnings
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerWarn()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_WARN, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "WARN : " + logString);
+                Assert.IsNull(sr.ReadLine());
+            }
+        }
+
+
+
+        ///<summary>
+        ///Test that SQL tracing traces errors, warnings and SQL
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerSQL()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_SQL, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "WARN : " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "S Q L: " + logString);
+                Assert.IsNull(sr.ReadLine());
+            }
+        }
+
+
+
+        ///<summary>
+        ///Test that Info tracing traces errors, warnings, SQL and Info
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerInfo()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_INFO, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "WARN : " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "S Q L: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "INFO : " + logString);
+                Assert.IsNull(sr.ReadLine());
+            }
+
+
+        }
+
+
+
+        ///<summary>
+        ///Test that Info tracing traces errors, warnings, SQL, Info and Extreme
+        ///</summary>
+        [TestMethod]
+        public void Test_FeatherLoggerExtreme()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            string logString = "log";
+
+            FeatherLogger fl = new FeatherLogger(FeatherLogger.TRACE_LEVEL_EXTREME, TEST_PATH + @"\" + testName, testName, false, "xml");
+
+            fl.Error(logString);
+            fl.Warn(logString);
+            fl.Sql(logString);
+            fl.Info(logString);
+            fl.Extreme(logString);
+
+
+            using (StreamReader sr = new StreamReader(TEST_PATH + @"\" + testName + @"\" + testName + ".xml"))
+            {
+                sr.ReadLine();
+
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "ERROR: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "WARN : " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "S Q L: " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "INFO : " + logString);
+                Assert.AreEqual(sr.ReadLine().Substring(0, 7 + logString.Length), "EXTRM: " + logString);
+                Assert.IsNull(sr.ReadLine());
+            }
+        }
+        
+
+    }
+}
