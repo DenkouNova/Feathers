045b6cc76d03dbfabec07d95bee30b06c31ce92e
fggallant <fggallant@gmail.com>
2016-10-10 09:47:42 -0400

    First commit, just a base

---
+++ FeatherStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Feathers
+{
+    public class FeatherStrings
+    {
+        public static string ReplaceWhitespaceSpecialCharacters(string s)
+        {
+            return s
+                .Replace("\r", @"{\r}")
+                .Replace("\n", @"{\n}")
+                .Replace("\t", @"{\t}");
+        }
+    }
+}
